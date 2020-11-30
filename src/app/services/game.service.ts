3b32794dcdad366debd27cffe6956f69d131a73e
MacSantaCruz <santacrm@oregonstate.edu>
2020-11-29 17:07:59 -0800

    Game Service to Env variable

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -8,15 +8,13 @@ import { Game } from '../models/game.model';
   providedIn: 'root'
 })
 export class GameService {
-  //apiUrl ='http://localhost:3001/api/';
-  //apiUrl = 'https://node-api-szmkxbu6aq-uw.a.run.app/api/'
   apiUrl = environment.apiURL;
   gameUrl = '/game/';
 
   constructor(private http: HttpClient) { }
 
   submitNewGame(newGame: Game):Observable<Game>{
-    return this.http.post<Game>("http://localhost:3001/api/game",newGame);
+    return this.http.post<Game>(this.apiUrl + this.gameUrl,newGame);
   }
 
 }
